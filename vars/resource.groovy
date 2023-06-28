f07fcee9700b4631ca98e83a79f95d52d5089fb7
HKMtech123 <122669298+HKMtech123@users.noreply.github.com>
2023-06-28 12:01:16 +0530

    Update resource.groovy

--- vars/resource.groovy
+++ vars/resource.groovy
@@ -2,6 +2,12 @@ def call()
 {
   def content = libraryResource "bash.sh"
   writeFile file: "bash.sh", text: content
+  sh 'ls -l ./bash.sh'
   sh "./bash.sh"
 }
 
+def call(Map config = [:]) { 
+  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"    
+  writeFile file: "${config.name}", text: scriptcontents 
+  sh "chmod a+x ./${config.name}"
+} 
